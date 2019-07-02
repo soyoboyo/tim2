b90cfbfeeab39bd709393cd04a439c3e6570496e
Jacek <jacekzalewski95@gmail.com>
2019-07-02 22:48:54 +0200

    Filter out only proper locale languages

--- backend/src/main/java/org/tim/services/PredefinedLocalesService.java
+++ backend/src/main/java/org/tim/services/PredefinedLocalesService.java
@@ -7,26 +7,28 @@ import java.util.*;
 @Service
 public class PredefinedLocalesService {
 
-    public LinkedHashSet<String> getPredefinedLanguages() {
-        Locale locales[] = Locale.getAvailableLocales();
-        LinkedHashSet<String> languages = new LinkedHashSet<>();
-        for(int i = 0; i < locales.length; i++){
-            if(!(locales[i].getLanguage().isEmpty() || locales[i].getCountry().isEmpty())) {
-                languages.add(locales[i].getLanguage() + " (" + locales[i].getDisplayLanguage() + ")");
-            }
-        }
-        return languages;
-    }
+	public LinkedHashSet<String> getPredefinedLanguages() {
+		Locale locales[] = Locale.getAvailableLocales();
+		LinkedHashSet<String> languages = new LinkedHashSet<>();
+		for (int i = 0; i < locales.length; i++) {
+			if (!(locales[i].getLanguage().isEmpty() || locales[i].getCountry().isEmpty())) {
+				if (locales[i].getLanguage().length() == 2) {
+					languages.add(locales[i].getLanguage() + " (" + locales[i].getDisplayLanguage() + ")");
+				}
+			}
+		}
+		return languages;
+	}
 
-    public LinkedHashSet<String> getPredefinedCounties() {
-        Locale locales[] = Locale.getAvailableLocales();
-        LinkedHashSet<String> countries = new LinkedHashSet<>();
-        for(int i = 0; i < locales.length; i++){
-            if(!(locales[i].getLanguage().isEmpty() || locales[i].getCountry().isEmpty() ||
-                    !locales[i].getDisplayCountry().matches("[A-Z][a-zA-Z ]{0,}[a-z]") ||
-                    !locales[i].getCountry().matches("[a-zA-Z]{1,}")))
-                countries.add(locales[i].getCountry() + " (" + locales[i].getDisplayCountry() + ")");
-        }
-        return countries;
-    }
+	public LinkedHashSet<String> getPredefinedCounties() {
+		Locale locales[] = Locale.getAvailableLocales();
+		LinkedHashSet<String> countries = new LinkedHashSet<>();
+		for (int i = 0; i < locales.length; i++) {
+			if (!(locales[i].getLanguage().isEmpty() || locales[i].getCountry().isEmpty() ||
+					!locales[i].getDisplayCountry().matches("[A-Z][a-zA-Z ]{0,}[a-z]") ||
+					!locales[i].getCountry().matches("[a-zA-Z]{1,}")))
+				countries.add(locales[i].getCountry() + " (" + locales[i].getDisplayCountry() + ")");
+		}
+		return countries;
+	}
 }
