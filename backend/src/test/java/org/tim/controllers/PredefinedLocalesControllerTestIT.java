14b6e4f2f40392d8cacb358b2ef5f6b49a761a12
Jacek <jacekzalewski95@gmail.com>
2019-07-08 22:35:02 +0200

    Maven failsafe plugin fix

--- backend/src/test/java/org/tim/controllers/PredefinedLocalesControllerTestIT.java
+++ backend/src/test/java/org/tim/controllers/PredefinedLocalesControllerTestIT.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.tim.utils.Mapping.*;
 
 @ExtendWith({SpringExtension.class})
-public class PredefinedLocalesControllerTestCT {
+public class PredefinedLocalesControllerTestIT {
 
 	private MockMvc mockMvc;
 	private static TreeSet<String> expectedPredefinedLanguages;
