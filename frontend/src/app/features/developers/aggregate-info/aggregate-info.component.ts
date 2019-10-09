566ed6ab9f4bec370ef89791e812c201d04620fb
Jacek <jacekzalewski95@gmail.com>
2019-10-09 23:06:00 +0200

    Sample chart set up

--- frontend/src/app/features/developers/aggregate-info/aggregate-info.component.ts
+++ frontend/src/app/features/developers/aggregate-info/aggregate-info.component.ts
@@ -1,32 +1,34 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { RestService } from '../../../shared/services/rest/rest.service';
 
+import { Chart } from 'chart.js';
 
-declare var Chart: any;
 
 @Component({
 	selector: 'app-aggregate-info',
 	templateUrl: './aggregate-info.component.html',
 	styleUrls: ['./aggregate-info.component.scss']
 })
-export class AggregateInfoComponent implements OnInit, OnChanges {
+export class AggregateInfoComponent implements OnInit, OnChanges, AfterViewInit {
 
 	@Input() selectedProjectId: number;
 	aggregatedInfoList: any;
-	aggregatedInfo = null;
+	aggregatedInfo: any = null;
 
-	histogramChartElement;
-	histogramChart;
-
-	histogramData: any = [];
-	histogramIndexes: any = [];
-	histogramValues: any = [];
-	histogramChartId = 'histogramChart';
+	summaryChartElement;
+	summaryChart;
+	summaryChartId = 'summaryChart';
 	labels: any = [];
+	dataCorrect: any = [];
+	dataIncorrect: any = [];
+
 	constructor(private http: RestService) {
 	}
 
 	ngOnInit() {
+		this.labels = ['label', 'srabel'];
+		this.dataCorrect = [10, 20];
+
 	}
 
 	ngOnChanges(changes: SimpleChanges): void {
@@ -41,20 +43,37 @@ export class AggregateInfoComponent implements OnInit, OnChanges {
 		}
 	}
 
+	ngAfterViewInit(): void {
+		this.summaryChartElement = document.getElementById(this.summaryChartId);
+		this.generateChart();
+	}
+
 	generateChart() {
-		this.histogramChart = new Chart(this.histogramChartElement, {
+		console.log('generage chart');
+		this.summaryChart = new Chart(this.summaryChartElement, {
 			type: 'bar',
 			data: {
-				labels: this.labels,
+				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
 				datasets: [{
-					label: 'Data',
-					data: this.histogramValues,
-					backgroundColor: 'rgba(0, 0, 160, 1)',
-					borderColor: 'rgba(0, 0, 160, 1)',
-					borderWidth: 1,
-					fill: false,
-					pointRadius: 1,
-					pointBorderWidth: 1
+					label: this.labels,
+					data: this.dataCorrect,
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 1
 				}]
 			},
 			options: {
@@ -64,17 +83,10 @@ export class AggregateInfoComponent implements OnInit, OnChanges {
 							beginAtZero: true
 						}
 					}]
-				},
-				spanGaps: false,
-				elements: {
-					line: {
-						skipNull: true,
-						drawNull: false,
-					}
 				}
 			}
 		});
-		this.showElement(this.histogramChartElement);
+		this.showElement(this.summaryChartElement);
 	}
 
 	switchOpenHideElement(element: any) {
@@ -93,4 +105,42 @@ export class AggregateInfoComponent implements OnInit, OnChanges {
 	showElement(element: any) {
 		element.style.display = 'block';
 	}
+
+
+
+	// generateChart() {
+	// 	this.summaryChart = new Chart(this.summaryChartElement, {
+	// 		type: 'bar',
+	// 		data: {
+	// 			labels: this.labels,
+	// 			datasets: [{
+	// 				label: 'Data',
+	// 				data: this.histogramValues,
+	// 				backgroundColor: 'rgba(0, 0, 160, 1)',
+	// 				borderColor: 'rgba(0, 0, 160, 1)',
+	// 				borderWidth: 1,
+	// 				fill: false,
+	// 				pointRadius: 1,
+	// 				pointBorderWidth: 1
+	// 			}]
+	// 		},
+	// 		options: {
+	// 			scales: {
+	// 				yAxes: [{
+	// 					ticks: {
+	// 						beginAtZero: true
+	// 					}
+	// 				}]
+	// 			},
+	// 			spanGaps: false,
+	// 			elements: {
+	// 				line: {
+	// 					skipNull: true,
+	// 					drawNull: false,
+	// 				}
+	// 			}
+	// 		}
+	// 	});
+	// 	this.showElement(this.summaryChartElement);
+	// }
 }
