46d39b8f0bd56dad1399dc84b450dd932f828cc1
Jacek <jacekzalewski95@gmail.com>
2019-10-09 22:22:51 +0200

    Get old chart setup

--- frontend/src/app/features/developers/aggregate-info/aggregate-info.component.ts
+++ frontend/src/app/features/developers/aggregate-info/aggregate-info.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { RestService } from '../../../shared/services/rest/rest.service';
 
+
+declare var Chart: any;
+
 @Component({
 	selector: 'app-aggregate-info',
 	templateUrl: './aggregate-info.component.html',
@@ -12,6 +15,14 @@ export class AggregateInfoComponent implements OnInit, OnChanges {
 	aggregatedInfoList: any;
 	aggregatedInfo = null;
 
+	histogramChartElement;
+	histogramChart;
+
+	histogramData: any = [];
+	histogramIndexes: any = [];
+	histogramValues: any = [];
+	histogramChartId = 'histogramChart';
+	labels: any = [];
 	constructor(private http: RestService) {
 	}
 
@@ -28,5 +39,58 @@ export class AggregateInfoComponent implements OnInit, OnChanges {
 			this.aggregatedInfo = response;
 			this.aggregatedInfoList = response.translationStatusesByLocale;
 		}
+	}
+
+	generateChart() {
+		this.histogramChart = new Chart(this.histogramChartElement, {
+			type: 'bar',
+			data: {
+				labels: this.labels,
+				datasets: [{
+					label: 'Data',
+					data: this.histogramValues,
+					backgroundColor: 'rgba(0, 0, 160, 1)',
+					borderColor: 'rgba(0, 0, 160, 1)',
+					borderWidth: 1,
+					fill: false,
+					pointRadius: 1,
+					pointBorderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero: true
+						}
+					}]
+				},
+				spanGaps: false,
+				elements: {
+					line: {
+						skipNull: true,
+						drawNull: false,
+					}
+				}
+			}
+		});
+		this.showElement(this.histogramChartElement);
+	}
+
+	switchOpenHideElement(element: any) {
+		if (element.style.display === 'none') {
+			element.style.display = 'block';
+		} else {
+			element.style.display = 'none';
+		}
+	}
+
+	hideElement(element: any) {
+		element.style.display = 'none';
+
+	}
+
+	showElement(element: any) {
+		element.style.display = 'block';
 	}
 }
