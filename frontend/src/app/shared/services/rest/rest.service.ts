f9d6f74db6324ec48822531dba71e7b66c5d816a
jaca1119 <laki1663@gmail.com>
2020-07-09 13:01:03 +0200

    T-01: Buttons to import data from CSV file

--- frontend/src/app/shared/services/rest/rest.service.ts
+++ frontend/src/app/shared/services/rest/rest.service.ts
@@ -1,29 +1,29 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/internal/operators';
-import { Observable, throwError } from 'rxjs';
-import { environment } from '../../../../environments/environment';
-import { saveAs } from 'file-saver';
-import { Project } from '../../types/entities/Project';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {catchError} from 'rxjs/internal/operators';
+import {Observable, throwError} from 'rxjs';
+import {environment} from '../../../../environments/environment';
+import {saveAs} from 'file-saver';
+import {Project} from '../../types/entities/Project';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class RestService {
-	URL = environment.API + '/api/v1/';
+  URL = environment.API + '/api/v1/';
 
-	constructor(private http: HttpClient) {
-	}
+  constructor(private http: HttpClient) {
+  }
 
-	async getAll(url: string) {
-		return await this.http.get<any>(this.URL + url, { withCredentials: true })
-		.pipe(
-			catchError(this.handleError)
-		).toPromise();
-	}
+  async getAll(url: string) {
+    return await this.http.get<any>(this.URL + url, {withCredentials: true})
+      .pipe(
+        catchError(this.handleError)
+      ).toPromise();
+  }
 
-	save(url: string, body: any): Observable<any> {
-		return this.http.post<any>(this.URL + url, body, { withCredentials: true })
+  save(url: string, body: any): Observable<any> {
+    return this.http.post<any>(this.URL + url, body, {withCredentials: true})
 		.pipe(
 			catchError(this.handleError)
 		);
@@ -44,31 +44,40 @@ export class RestService {
 	}
 
 	async downloadXLS(project: Project) {
-		const url = this.URL + 'message/export/getByProject/' + project.id;
-		const d = new Date();
-		const now = d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear() + " " +
-			d.getHours() + ":" + d.getMinutes();
-		const filename = 'report_' + project.name + '_' + now + '.xlsx';
+    const url = this.URL + 'message/export/getByProject/' + project.id;
+    const d = new Date();
+    const now = d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear() + " " +
+      d.getHours() + ":" + d.getMinutes();
+    const filename = 'report_' + project.name + '_' + now + '.xlsx';
 
-		await this.http.get(url, {
-			responseType: 'blob',
-			withCredentials: true
-		}).subscribe(response => {
-			let blob = new Blob([response], { type: 'application/xml' });
-			saveAs(blob, filename);
-		});
+    await this.http.get(url, {
+      responseType: 'blob',
+      withCredentials: true
+    }).subscribe(response => {
+      let blob = new Blob([response], {type: 'application/xml'});
+      saveAs(blob, filename);
+    });
 
-	}
+  }
 
+  importCSV(url: string, file: File) {
+    let input = new FormData();
+    input.append("file", file);
 
-	private handleError(error: HttpErrorResponse) {
-		if (error.error instanceof ErrorEvent) {
-			console.error('An error occurred:', error.error.message);
-		} else {
-			console.error(
-				`Backend returned code ${error.status}, ` +
-				`exception was: ${error.error.message}`);
-		}
-		return throwError(error);
-	}
+    return this.http.post(this.URL + url, input, {
+      responseType: "text",
+      withCredentials: true
+    });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `exception was: ${error.error.message}`);
+    }
+    return throwError(error);
+  }
 }
