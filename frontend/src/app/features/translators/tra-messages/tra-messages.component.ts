4ccf47fabf6490a997aedf42fdc54f2a3479b0f2
Jacek <jacekzalewski95@gmail.com>
2020-07-21 20:36:09 +0200

    T03: Clean up unused methods and styles from tra-messages.component and translator-form.component

--- frontend/src/app/features/translators/tra-messages/tra-messages.component.ts
+++ frontend/src/app/features/translators/tra-messages/tra-messages.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl } from '@angular/forms';
 import { RestService } from '../../../shared/services/rest/rest.service';
 import { SnackbarService } from '../../../shared/services/snackbar-service/snackbar.service';
 import { UtilsService } from '../../../shared/services/utils-service/utils.service';
@@ -7,7 +7,6 @@ import { ProjectsStoreService } from '../../../stores/projects-store/projects-st
 import { ConfirmationDialogService } from '../../../shared/services/confirmation-dialog/confirmation-dialog.service';
 import { Message } from '../../../shared/types/entities/Message';
 import { Project } from '../../../shared/types/entities/Project';
-import { MessageForTranslator } from '../../../shared/types/DTOs/output/MessageForTranslator';
 
 @Component({
 	selector: 'app-tra-messages',
@@ -16,23 +15,14 @@ import { MessageForTranslator } from '../../../shared/types/DTOs/output/MessageF
 })
 export class TraMessagesComponent implements OnInit {
 
-	messageParams: FormGroup;
-	formMode = 'Add';
-	toUpdate: any = null;
-	showForm = false;
-
 	isLoadingResults = true;
-	selectedRowIndex = -1;
-
 	projects: Project[] = [];
 	messages: Message[] = [];
 
 	selectedProject = null;
 	selectedLocale: string = null;
-	selectedMessage = null;
 	availableLocales: any[] = [];
 	selectedLocales = new FormControl();
-	private selectedTranslationId: number;
 
 	constructor(private formBuilder: FormBuilder,
 				private cd: ChangeDetectorRef,
@@ -46,51 +36,9 @@ export class TraMessagesComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		this.initProjectForm();
 		this.getProjects();
 		this.getMessagesForTranslator();
 	}
-
-	initProjectForm() {
-		this.messageParams = this.formBuilder.group({
-			content: ['', [Validators.required]]
-		});
-	}
-
-	// createTranslation(params: any) {
-	// 	if (!this.toUpdate) {
-	// 		this.addTranslation(new TranslationCreateDTO(params.value.content, this.selectedLocale));
-	// 	} else {
-	// 		this.updateTranslation(new TranslationUpdateDTO(params.value.content));
-	// 	}
-	// }
-
-	addNewTranslation(message: MessageForTranslator) {
-		this.selectedMessage = message;
-		this.selectedRowIndex = message.id;
-
-		if (this.showForm === false) {
-			this.showForm = true;
-		}
-	}
-
-	editTranslation(message: MessageForTranslator) {
-		this.selectedTranslationId = message.translation.id;
-		this.selectedMessage = message;
-		this.selectedRowIndex = message.id;
-
-		if (this.showForm === false) {
-			this.showForm = true;
-		}
-
-		this.messageParams.patchValue({
-			content: message.translation.content,
-		});
-
-		this.toUpdate = message;
-		this.formMode = 'Update';
-	}
-
 
 	async getProjects() {
 		this.isLoadingResults = true;
@@ -99,7 +47,6 @@ export class TraMessagesComponent implements OnInit {
 	}
 
 	changeProject(value) {
-		this.cancelUpdate();
 		this.selectedProject = value;
 		this.projectStoreService.setSelectedProject(value);
 		this.getMessagesForTranslator();
@@ -147,22 +94,6 @@ export class TraMessagesComponent implements OnInit {
 		} else {
 			return o1.name === o2.name;
 		}
-	}
-
-	cancelUpdate() {
-		this.selectedMessage = null;
-		this.toUpdate = null;
-		this.selectedRowIndex = -1;
-		this.formMode = 'Add';
-		this.showForm = false;
-		this.clearForm();
-	}
-
-	clearForm() {
-		this.messageParams.reset();
-		this.messageParams.markAsPristine();
-		this.messageParams.markAsUntouched();
-		this.cd.markForCheck();
 	}
 
 	formAction(result: boolean) {
--- frontend/src/app/features/translators/tra-messages/translator-form/translator-form.component.ts
+++ frontend/src/app/features/translators/tra-messages/translator-form/translator-form.component.ts
@@ -4,9 +4,6 @@ import { TranslationCreateDTO } from '../../../../shared/types/DTOs/input/Transl
 import { TranslationUpdateDTO } from '../../../../shared/types/DTOs/input/TranslationUpdateDTO';
 import { RestService } from '../../../../shared/services/rest/rest.service';
 import { SnackbarService } from '../../../../shared/services/snackbar-service/snackbar.service';
-import { ConfirmationDialogService } from '../../../../shared/services/confirmation-dialog/confirmation-dialog.service';
-import { Project } from '../../../../shared/types/entities/Project';
-import { Message } from '../../../../shared/types/entities/Message';
 import { TranslatorFormStateService } from '../../translator-form-state-service/translator-form-state.service';
 
 @Component({
@@ -17,19 +14,6 @@ import { TranslatorFormStateService } from '../../translator-form-state-service/
 export class TranslatorFormComponent implements OnInit {
 
 	translationForm: FormGroup;
-	toUpdate: any = null;
-
-
-	isLoadingResults = true;
-	selectedRowIndex = -1;
-
-	projects: Project[] = [];
-	messages: Message[] = [];
-
-	selectedProject = null;
-	selectedLocale: string = null;
-	selectedMessage = null;
-	private selectedTranslationId: number;
 	@Input() formMode: string;
 	@Output() hideForm = new EventEmitter<boolean>();
 	@Output() formSubmitted = new EventEmitter<boolean>();
@@ -38,12 +22,13 @@ export class TranslatorFormComponent implements OnInit {
 				private cd: ChangeDetectorRef,
 				private http: RestService,
 				private snackbar: SnackbarService,
-				private confirmService: ConfirmationDialogService,
 				private formState: TranslatorFormStateService) {
 	}
 
 	ngOnInit(): void {
-		this.initTranslationForm();
+		this.translationForm = this.formBuilder.group({
+			content: ['', [Validators.required]]
+		});
 		console.log(this.formMode);
 		if (this.formMode === 'Update') {
 			this.translationForm.patchValue({
@@ -52,13 +37,21 @@ export class TranslatorFormComponent implements OnInit {
 		}
 	}
 
-	initTranslationForm() {
-		this.translationForm = this.formBuilder.group({
-			content: ['', [Validators.required]]
-		});
+	// form actions
+	cancelForm() {
+		this.clearForm();
+		this.hideForm.emit(false);
+		this.formState.closeForm();
 	}
 
-	createTranslation(params: any) {
+	clearForm() {
+		this.translationForm.reset();
+		this.translationForm.markAsPristine();
+		this.translationForm.markAsUntouched();
+		this.cd.markForCheck();
+	}
+
+	submitTranslationForm(params: any) {
 		if (this.formMode === 'Add') {
 			this.addTranslation(new TranslationCreateDTO(params.value.content, this.formState.getLocale()));
 		} else {
@@ -66,6 +59,7 @@ export class TranslatorFormComponent implements OnInit {
 		}
 	}
 
+	// REST api actions
 	async addTranslation(body) {
 		this.http.save('translation/create' + '?messageId=' + this.formState.getMessage().id, body).subscribe((response) => {
 			this.submitNormal(response);
@@ -85,35 +79,20 @@ export class TranslatorFormComponent implements OnInit {
 
 	submitNormal(response: any) {
 		if (response !== null) {
-			this.formSubmitted.emit(true);
-			this.snackbar.snackSuccess('Success!', 'OK');
-			this.clearForm();
 			this.hideForm.emit(true);
+			this.formSubmitted.emit(true);
+			this.clearForm();
+			this.snackbar.snackSuccess('Success!', 'OK');
 		} else {
-			this.formSubmitted.emit(false);
 			this.hideForm.emit(false);
+			this.formSubmitted.emit(false);
 			this.snackbar.snackError('Error', 'OK');
 		}
 	}
 
 	submitException(error: any) {
-		this.formSubmitted.emit(false);
 		this.hideForm.emit(false);
+		this.formSubmitted.emit(false);
 		this.snackbar.snackError(error.error.message, 'OK');
 	}
-	
-	cancelForm() {
-		this.clearForm();
-	}
-
-	clearForm() {
-		this.hideForm.emit(false);
-		this.formState.closeForm();
-		this.translationForm.reset();
-		this.translationForm.markAsPristine();
-		this.translationForm.markAsUntouched();
-		this.cd.markForCheck();
-	}
-
-
 }
