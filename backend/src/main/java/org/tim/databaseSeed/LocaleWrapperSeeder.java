30fa84b29fcae05283a980151ece1f38872862ec
Jacek <jacekzalewski95@gmail.com>
2020-08-19 17:01:03 +0200

    T12: Move remaining messages to json files for projects P1 and P4, remove uuid if it's the same as key

--- backend/src/main/java/org/tim/databaseSeed/LocaleWrapperSeeder.java
+++ backend/src/main/java/org/tim/databaseSeed/LocaleWrapperSeeder.java
@@ -18,26 +18,13 @@ public class LocaleWrapperSeeder {
 	public Map<String, LocaleWrapper> initLocales() {
 		Map<String, LocaleWrapper> locales = new HashMap<>();
 
-		LocaleWrapper en_US = new LocaleWrapper(Locale.US);
-		locales.put("en_US", localeWrapperRepository.save(en_US));
-
-		LocaleWrapper en_GB = new LocaleWrapper(Locale.UK);
-		locales.put("en_GB", localeWrapperRepository.save(en_GB));
-
-		LocaleWrapper pl_PL = new LocaleWrapper(new Locale("pl", "PL"));
-		locales.put("pl_PL", localeWrapperRepository.save(pl_PL));
-
-		LocaleWrapper de_DE = new LocaleWrapper(Locale.GERMANY);
-		locales.put("de_DE", localeWrapperRepository.save(de_DE));
-
-		LocaleWrapper ar_LY = new LocaleWrapper(new Locale("ar", "LY"));
-		locales.put("ar_LY", localeWrapperRepository.save(ar_LY));
-
-		LocaleWrapper ko_KR = new LocaleWrapper(new Locale("ko", "KR"));
-		locales.put("ko_KR", localeWrapperRepository.save(ko_KR));
-
-		LocaleWrapper pl_DE = new LocaleWrapper(new Locale("pl", "DE"));
-		locales.put("pl_DE", localeWrapperRepository.save(pl_DE));
+		locales.put("en_US", localeWrapperRepository.save(new LocaleWrapper(Locale.US)));
+		locales.put("en_GB", localeWrapperRepository.save(new LocaleWrapper(Locale.UK)));
+		locales.put("pl_PL", localeWrapperRepository.save(new LocaleWrapper(new Locale("pl", "PL"))));
+		locales.put("de_DE", localeWrapperRepository.save(new LocaleWrapper(Locale.GERMANY)));
+		locales.put("ar_LY", localeWrapperRepository.save(new LocaleWrapper(new Locale("ar", "LY"))));
+		locales.put("ko_KR", localeWrapperRepository.save(new LocaleWrapper(new Locale("ko", "KR"))));
+		locales.put("pl_DE", localeWrapperRepository.save(new LocaleWrapper(new Locale("pl", "DE"))));
 
 		locales.put("af_NA", localeWrapperRepository.save(new LocaleWrapper(new Locale("af", "NA"))));
 		locales.put("af_ZA", localeWrapperRepository.save(new LocaleWrapper(new Locale("af", "ZA"))));
--- backend/src/main/java/org/tim/databaseSeed/MessageSeeder.java
+++ backend/src/main/java/org/tim/databaseSeed/MessageSeeder.java
@@ -41,136 +41,14 @@ public class MessageSeeder {
 				String description = (String) m.get("description");
 				Message message = new Message(key, content, projects.get("project" + id));
 				message.setDescription(description);
-				messages.put((String) m.get("uuid"), messageRepository.save(message));
+				String uuid = (String) m.get("uuid");
+				if (uuid != null) {
+					messages.put(uuid, messageRepository.save(message));
+				} else {
+					messages.put(key, messageRepository.save(message));
+				}
 			}
 		}
-
-
-		Message proEmailSupport = new Message("proEmailSupport", "Priority email support", projects.get("projectP1"));
-		proEmailSupport.setDescription("Information about priority email support for pro account.");
-		messages.put("proEmailSupport", messageRepository.save(proEmailSupport));
-		Message proHelpCenter = new Message("proHelpCenter", "Help center access", projects.get("projectP1"));
-		proHelpCenter.setDescription("Information that user can use free help center for pro account.");
-		messages.put("proHelpCenter", messageRepository.save(proHelpCenter));
-		Message getStarted = new Message("getStarted", "Get started", projects.get("projectP1"));
-		getStarted.setDescription("Information that user need to create account to use this product.");
-		messages.put("getStarted", messageRepository.save(getStarted));
-		Message enterpriseCard = new Message("enterpriseCard", "Enterprise", projects.get("projectP1"));
-		enterpriseCard.setDescription("Information about product type.");
-		messages.put("enterpriseCard", messageRepository.save(enterpriseCard));
-		Message maxEnterpriseUsers = new Message("maxEnterpriseUsers", "30 users included", projects.get("projectP1"));
-		maxEnterpriseUsers.setDescription("Information about maximal number of user that can use this product on enterprise type.");
-		messages.put("maxEnterpriseUsers", messageRepository.save(maxEnterpriseUsers));
-		Message maxEnterpriseMemoryLimit = new Message("maxEnterpriseMemoryLimit", "15 GB of storage", projects.get("projectP1"));
-		maxEnterpriseMemoryLimit.setDescription("Information about maximum memory limit that can be use on enterprise type.");
-		messages.put("maxEnterpriseMemoryLimit", messageRepository.save(maxEnterpriseMemoryLimit));
-		Message enterpriseEmailSupport = new Message("enterpriseEmailSupport", "Phone and email support", projects.get("projectP1"));
-		enterpriseEmailSupport.setDescription("Information about phone and email support for enterprise account.");
-		messages.put("enterpriseEmailSupport", messageRepository.save(enterpriseEmailSupport));
-		Message enterpriseHelpCenter = new Message("enterpriseHelpCenter", "Help center access", projects.get("projectP1"));
-		enterpriseHelpCenter.setDescription("Information that user can use free help center for pro account.");
-		messages.put("enterpriseHelpCenter", messageRepository.save(enterpriseHelpCenter));
-		Message contactUs = new Message("contactUs", "Contact us", projects.get("projectP1"));
-		contactUs.setDescription("Information that user need to create account to use this product.");
-		messages.put("contactUs", messageRepository.save(contactUs));
-		Message featuresFooter = new Message("featuresFooter", "Features", projects.get("projectP1"));
-		featuresFooter.setDescription("Footer links");
-		messages.put("featuresFooter", messageRepository.save(featuresFooter));
-		Message featuresFooter1 = new Message("featuresFooter1", "Cool stuff", projects.get("projectP1"));
-		featuresFooter1.setDescription("Footer links");
-		messages.put("featuresFooter1", messageRepository.save(featuresFooter1));
-		Message featuresFooter2 = new Message("featuresFooter2", "Random feature", projects.get("projectP1"));
-		featuresFooter2.setDescription("Footer links");
-		messages.put("featuresFooter2", messageRepository.save(featuresFooter2));
-		Message featuresFooter3 = new Message("featuresFooter3", "Team feature", projects.get("projectP1"));
-		featuresFooter3.setDescription("Footer links");
-		messages.put("featuresFooter3", messageRepository.save(featuresFooter3));
-		Message featuresFooter4 = new Message("featuresFooter4", "Stuff for developers", projects.get("projectP1"));
-		featuresFooter4.setDescription("Footer links");
-		messages.put("featuresFooter4", messageRepository.save(featuresFooter4));
-		Message featuresFooter5 = new Message("featuresFooter5", "Another one", projects.get("projectP1"));
-		featuresFooter5.setDescription("Footer links");
-		messages.put("featuresFooter5", messageRepository.save(featuresFooter5));
-		Message featuresFooter6 = new Message("featuresFooter6", "Last time", projects.get("projectP1"));
-		featuresFooter6.setDescription("Footer links");
-		messages.put("featuresFooter6", messageRepository.save(featuresFooter6));
-		Message resourcesFooter = new Message("resourcesFooter", "Resources", projects.get("projectP1"));
-		resourcesFooter.setDescription("Footer links");
-		messages.put("resourcesFooter", messageRepository.save(resourcesFooter));
-		Message resourcesFooter1 = new Message("resourcesFooter1", "Resource", projects.get("projectP1"));
-		resourcesFooter1.setDescription("Footer links");
-		messages.put("resourcesFooter1", messageRepository.save(resourcesFooter1));
-		Message resourcesFooter2 = new Message("resourcesFooter2", "Resource name", projects.get("projectP1"));
-		resourcesFooter2.setDescription("Footer links");
-		messages.put("resourcesFooter2", messageRepository.save(resourcesFooter2));
-		Message resourcesFooter3 = new Message("resourcesFooter3", "Another resource", projects.get("projectP1"));
-		resourcesFooter3.setDescription("Footer links");
-		messages.put("resourcesFooter3", messageRepository.save(resourcesFooter3));
-		Message resourcesFooter4 = new Message("resourcesFooter4", "Final resource", projects.get("projectP1"));
-		resourcesFooter4.setDescription("Footer links");
-		messages.put("resourcesFooter4", messageRepository.save(resourcesFooter4));
-		Message aboutFooter = new Message("aboutFooter", "About", projects.get("projectP1"));
-		aboutFooter.setDescription("Footer links");
-		messages.put("aboutFooter", messageRepository.save(aboutFooter));
-		Message aboutFooter1 = new Message("aboutFooter1", "Team", projects.get("projectP1"));
-		aboutFooter1.setDescription("Footer links");
-		messages.put("aboutFooter1", messageRepository.save(aboutFooter1));
-		Message aboutFooter2 = new Message("aboutFooter2", "Locations", projects.get("projectP1"));
-		aboutFooter2.setDescription("Footer links");
-		messages.put("aboutFooter2", messageRepository.save(aboutFooter2));
-		Message aboutFooter3 = new Message("aboutFooter3", "Privacy", projects.get("projectP1"));
-		aboutFooter3.setDescription("Footer links");
-		messages.put("aboutFooter3", messageRepository.save(aboutFooter3));
-		Message aboutFooter4 = new Message("aboutFooter4", "Terms", projects.get("projectP1"));
-		aboutFooter4.setDescription("Footer links");
-		messages.put("aboutFooter4", messageRepository.save(aboutFooter4));
-
-
-// project 4
-		Message categoriesTitle = new Message("categoriesTitle", "Categories", projects.get("projectP4"));
-		categoriesTitle.setDescription("Categories title");
-		messages.put("categoriesTitle", messageRepository.save(categoriesTitle));
-		Message catHome = new Message("categoryHome", "Home", projects.get("projectP4"));
-		messages.put("categoryHome", messageRepository.save(catHome));
-		Message catEl = new Message("categoryElectronics", "Electronics", projects.get("projectP4"));
-		messages.put("categoryElectronics", messageRepository.save(catEl));
-		Message catVid = new Message("categoryVideoGames", "Video Games", projects.get("projectP4"));
-		messages.put("categoryVideoGames", messageRepository.save(catVid));
-		Message categorySel = new Message("selectedCategory", "Selected category", projects.get("projectP4"));
-		messages.put("selectedCategory", messageRepository.save(categorySel));
-		Message hotProduct = new Message("hotProduct", "Hot product", projects.get("projectP4"));
-		messages.put("hotProduct", messageRepository.save(hotProduct));
-		Message hotProductContent = new Message("hotProductContent", "<b>HOT</b> Phone XYZ only for <b>100$</b>", projects.get("projectP4"));
-		messages.put("hotProductContent", messageRepository.save(hotProductContent));
-		Message addToCart = new Message("addToCart", "Add to cart", projects.get("projectP4"));
-		messages.put("addToCart", messageRepository.save(addToCart));
-		Message featuredProducts = new Message("featuredProducts", "Featured products", projects.get("projectP4"));
-		messages.put("featuredProducts", messageRepository.save(featuredProducts));
-		Message productContent1 = new Message("productContent1", "Watch XYZ <b>90$</b>", projects.get("projectP4"));
-		messages.put("productContent1", messageRepository.save(productContent1));
-		Message productContent2 = new Message("productContent2", "Chair XYZ <b>40$</b>", projects.get("projectP4"));
-		messages.put("productContent2", messageRepository.save(productContent2));
-		Message productContent3 = new Message("productContent3", "Game console XYZ <b>60$</b>", projects.get("projectP4"));
-		messages.put("productContent3", messageRepository.save(productContent3));
-		Message showMore = new Message("showMore", "Show more", projects.get("projectP4"));
-		messages.put("showMore", messageRepository.save(showMore));
-
-		Message help = new Message("help", "Help", projects.get("projectP4"));
-		messages.put("help", messageRepository.save(help));
-		Message yOrders = new Message("yOrders", "Your orders", projects.get("projectP4"));
-		messages.put("yOrders", messageRepository.save(yOrders));
-		Message yAccount = new Message("yAccount", "Your account", projects.get("projectP4"));
-		messages.put("yAccount", messageRepository.save(yAccount));
-		Message helpCenter = new Message("helpCenter", "Help center", projects.get("projectP4"));
-		messages.put("helpCenter", messageRepository.save(helpCenter));
-		Message aboutUs = new Message("aboutUs", "About Us", projects.get("projectP4"));
-		messages.put("aboutUs", messageRepository.save(aboutUs));
-		Message careers = new Message("careers", "Careers", projects.get("projectP4"));
-		messages.put("careers", messageRepository.save(careers));
-		Message aboutExample = new Message("aboutExample", "About Example App", projects.get("projectP4"));
-		messages.put("aboutExample", messageRepository.save(aboutExample));
-		Message blog = new Message("blog", "Our blog", projects.get("projectP4"));
-		messages.put("blog", messageRepository.save(blog));
 
 		return messages;
 	}
