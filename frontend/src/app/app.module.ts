3b14b1fc070499e2e8c0ca99ebc7f8cbca273ffb
Jacek <jacekzalewski95@gmail.com>
2020-08-25 18:15:49 +0200

    T0: Put declarations of developers and translators components in separate modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,51 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { routes } from './routes';
 import { NavbarComponent } from './layout/navbar/navbar.component';
-import { DevelopersComponent } from './features/developers/developers.component';
-import { TranslatorsComponent } from './features/translators/translators.component';
-import { DevProjectsComponent } from './features/developers/dev-projects/dev-projects.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { DevMessagesComponent } from './features/developers/dev-messages/dev-messages.component';
-import { DevMessagesTableComponent } from './features/developers/dev-messages/dev-messages-table/dev-messages-table.component';
 import { SnackbarService } from './shared/services/snackbar-service/snackbar.service';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { UtilsService } from './shared/services/utils-service/utils.service';
 import { ConfirmationDialogComponent } from './shared/components/confirmation-dialog/confirmation-dialog.component';
-import { DevProjectsTableComponent } from './features/developers/dev-projects/dev-projects-table/dev-projects-table.component';
 import { LoginComponent } from './core/login-component/login.component';
 import { LoginService } from './core/login-service/login.service';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { TraMessagesComponent } from './features/translators/tra-messages/tra-messages.component';
-import { TraMessagesTableComponent } from './features/translators/tra-messages/tra-messages-table/tra-messages-table.component';
-import { DevHistoryTranslationsComponent } from './features/developers/dev-history-translations/dev-history-translations.component';
-import { DevHistoryMessagesComponent } from './features/developers/dev-history-messages/dev-history-messages.component';
 import { ViewsModule } from './views/views.module';
 import { SharedModule } from './shared/shared.module';
-import { AggregateInfoComponent } from './features/developers/aggregate-info/aggregate-info.component';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthInterceptor } from './core/AuthInterceptor';
-import { TranslatorFormComponent } from './features/translators/tra-messages/translator-form/translator-form.component';
-import { DevProjectFormComponent } from './features/developers/dev-messages/dev-project-form/dev-project-form.component';
+import { DevelopersModule } from './features/developers/developers.module';
+import { TranslatorsModule } from './features/translators/translators.module';
 
 @NgModule({
 	declarations: [
-		AggregateInfoComponent,
 		AppComponent,
-		DevHistoryMessagesComponent,
-		DevHistoryTranslationsComponent,
-		DevMessagesComponent,
-		DevMessagesTableComponent,
-		DevProjectsComponent,
-		DevProjectsTableComponent,
-		DevelopersComponent,
 		LoginComponent,
-		NavbarComponent,
-		TraMessagesComponent,
-		TraMessagesTableComponent,
-		TranslatorFormComponent,
-		TranslatorsComponent,
-		DevProjectFormComponent
+		NavbarComponent
 	],
 	imports: [
 		BrowserAnimationsModule,
@@ -64,7 +40,9 @@ import { DevProjectFormComponent } from './features/developers/dev-messages/dev-
 		ReactiveFormsModule,
 		RouterModule.forRoot(routes),
 		SharedModule,
-		ViewsModule
+		ViewsModule,
+		DevelopersModule,
+		TranslatorsModule
 	],
 	exports: [
 		FormsModule,
---
+++ frontend/src/app/features/translators/translators.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TraMessagesComponent } from './tra-messages/tra-messages.component';
+import { TraMessagesTableComponent } from './tra-messages/tra-messages-table/tra-messages-table.component';
+import { TranslatorFormComponent } from './tra-messages/translator-form/translator-form.component';
+import { TranslatorsComponent } from './translators.component';
+import { MaterialModule } from '../../material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { routes } from '../../routes';
+import { SharedModule } from '../../shared/shared.module';
+import { ViewsModule } from '../../views/views.module';
+import { LoginService } from '../../core/login-service/login.service';
+import { UtilsService } from '../../shared/services/utils-service/utils.service';
+import { SnackbarService } from '../../shared/services/snackbar-service/snackbar.service';
+import { CookieService } from 'ngx-cookie-service';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { AuthInterceptor } from '../../core/AuthInterceptor';
+import { ConfirmationDialogComponent } from '../../shared/components/confirmation-dialog/confirmation-dialog.component';
+
+
+@NgModule({
+	declarations: [
+		TraMessagesComponent,
+		TraMessagesTableComponent,
+		TranslatorFormComponent,
+		TranslatorsComponent
+	],
+	imports: [
+		CommonModule,
+		BrowserAnimationsModule,
+		BrowserModule,
+		FlexLayoutModule,
+		FormsModule,
+		HttpClientModule,
+		MaterialModule,
+		ReactiveFormsModule,
+		ReactiveFormsModule,
+		RouterModule.forRoot(routes),
+		SharedModule,
+		ViewsModule
+	],
+	providers: [LoginService, UtilsService, SnackbarService, CookieService,
+		{ provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 5000 } },
+		{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+	entryComponents: [ConfirmationDialogComponent]
+})
+export class TranslatorsModule {
+}
